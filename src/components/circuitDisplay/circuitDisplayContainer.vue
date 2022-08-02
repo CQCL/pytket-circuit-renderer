19b2bed2b1775a5e603d67e0a0a18e5ec0888221
tiffanyduneau <tiffany@incubiq.com>
2022-08-02 13:37:45 +0100

    [MUS-1357] Add image export functionality

--- src/components/circuitDisplay/circuitDisplayContainer.vue
+++ src/components/circuitDisplay/circuitDisplayContainer.vue
@@ -1,10 +1,14 @@
 <script>
 import circuitDisplay from "./circuitDisplay";
+import infoModal from "./infoModal";
+import exportImage from "@/components/exportImage/exportImage";
 
 export default  {
   name: "circuit-display-container",
   components: {
     circuitDisplay,
+    infoModal,
+    exportImage,
   },
   props: {
     circuitRaw: {type: Object, default: undefined},
@@ -24,6 +28,12 @@ export default  {
         zoomOut: -0.1,
         zoomIn: 0.1,
       },
+      circuitEl: undefined,
+      circuitDimensions: {
+        width: undefined,
+        height: undefined,
+      },
+      exportImageModal: false, // toggle export dialog
       options: {
         zxStyle: {
           title: "Render gates using zx styles",
@@ -49,6 +59,10 @@ export default  {
           title: "Zoom out",
           icon: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-zoom-out" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/><path d="M10.344 11.742c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1 6.538 6.538 0 0 1-1.398 1.4z"/><path fill-rule="evenodd" d="M3 6.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"/></svg>`
         },
+        save: {
+          title: "Export",
+          icon: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16"><path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/><path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/></svg>`
+        }
       },
     };
   },
@@ -106,6 +120,17 @@ export default  {
       } else if (e.deltaY > 0) {
         this.zoom = Math.max(0.2, this.zoom - e.deltaY * 0.01)
       }
+    },
+    prepareExport () {
+      this.$refs.imageExport.resetState();
+      const {circuit, width, height} = this.$refs.circuitDisplay.getRenderedCircuitEl();
+      this.circuitEl = circuit;
+      this.circuitDimensions.width = width;
+      this.circuitDimensions.height = height;
+      this.exportImageModal = true;
+    },
+    modalContentUpdate (targetRef) {
+      this.$refs[targetRef].onResize();
     }
   }
 }
@@ -134,20 +159,40 @@ export default  {
              v-html="options[option].icon">
         </div>
       </div>
+      <div>
+        <div :title="options['save'].title"
+             class="icon"
+             role="button"
+             @click="prepareExport"
+             @keyup.space="prepareExport"
+             v-html="options['save'].icon">
+        </div>
+      </div>
     </div>
     <div :style="zoomStyling">
-      <circuit-display style="flex-grow: 1" :circuit="circuit" :render-options="renderOptions"></circuit-display>
+      <circuit-display ref="circuitDisplay" style="flex-grow: 1" :circuit="circuit" :render-options="renderOptions"></circuit-display>
     </div>
+    <info-modal v-model="exportImageModal" ref="imageExportModal">
+      <template #title>Export circuit as an image</template>
+      <template #content>
+        <export-image ref="imageExport"
+                      default-file-name="circuit"
+                      :element-to-render="circuitEl"
+                      :base-dimensions="circuitDimensions"
+                      @updated="modalContentUpdate('imageExportModal')">
+        </export-image>
+      </template>
+    </info-modal>
   </div>
 </template>
 
 <style scoped>
 .circuit-display-container {
-  min-height: 13em;
+  min-height: 3em;
   margin: 1em;
   display: flex;
   position: relative;
-  padding-left: 2.5em;
+  padding-top: 2.5em;
   font-family: "roboto", system-ui, sans-serif;
   --accent-col: green;
   --accent-col-fill: darkseagreen;
@@ -164,15 +209,15 @@ export default  {
 
 .display-options-container {
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-content: flex-start;
   justify-content: flex-start;
   align-items: center;
-  border-right: 1px solid var(--mid-col);
-  padding-right: 0.5em;
+  border-bottom: 1px solid var(--mid-col);
+  padding-bottom: 0.5em;
   position: absolute;
   top: 0;
-  bottom: 0;
+  right: 0;
   left: 0;
 }
 </style>
@@ -180,7 +225,7 @@ export default  {
 <style lang="scss">
 .circuit-display-container {
   .icon {
-    margin-bottom: 0.3em;
+    margin-right: 0.3em;
     padding: 0.4em;
     height: 1em;
     width: 1em;
