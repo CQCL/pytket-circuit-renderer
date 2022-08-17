1161fa399bb6fac4830266996d924eec5d1fec01
tiffanyduneau <tiffany@incubiq.com>
2022-08-17 10:47:11 +0200

    configure cypress and storybook

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  component: {
+    devServer: {
+      framework: 'vue-cli',
+      bundler: 'webpack',
+      webpackConfig: require('@vue/cli-service/webpack.config.js')
+    }
+  }
+})
