1ed90c9fbf4fd6fd66a05aa818886b647228cac3
tiffany.duneau <tiffany@incubiq.com>
2022-11-14 10:46:56 +0000

    dark theme - add init render options to story, detect system theme

--- src/components/circuitDisplay/circuitDisplayContainer.stories.js
+++ src/components/circuitDisplay/circuitDisplayContainer.stories.js
@@ -15,7 +15,13 @@ export default {
     }
   },
   args: {
-    circuitPreset: 'default'
+    circuitPreset: 'default',
+    initOptions: false,
+    darkTheme: false,
+    condensed: false,
+    condenseCBits: true,
+    zxStyle: true,
+    recursive: false
   }
 }
 
@@ -23,22 +29,41 @@ export const FromRaw = (args) => ({
   components: { CircuitDisplayContainer },
   setup () {
     return {
-      circuitRaw: CircuitStories[args.circuitPreset].args.circuit
+      circuitRaw: CircuitStories[args.circuitPreset].args.circuit,
+      initRenderOptions: args.initOptions
+        ? {
+            darkTheme: args.darkTheme,
+            condensed: args.condensed,
+            condenseCBits: args.condenseCBits,
+            zxStyle: args.zxStyle,
+            recursive: args.recursive
+          }
+        : {}
     }
   },
-  template: '<circuit-display-container :circuit-raw="circuitRaw"></circuit-display-container>'
+  template: '<circuit-display-container :circuit-raw="circuitRaw" :init-render-options="initRenderOptions"></circuit-display-container>'
 })
 
 export const FromDOM = (args) => ({
   components: { CircuitDisplayContainer },
   setup () {
     return {
-      circuitElementStr: '#circuitJSON'
+      circuitElementStr: '#circuitJSON',
+      initRenderOptions: args.initOptions
+        ? {
+            darkTheme: args.darkTheme,
+            condensed: args.condensed,
+            condenseCBits: args.condenseCBits,
+            zxStyle: args.zxStyle,
+            recursive: args.recursive
+          }
+        : {}
     }
   },
   template: `<div>
       <div style="position: relative; width: 100%; height: 400px">
-        <circuit-display-container :circuit-element-str="circuitElementStr"></circuit-display-container>
+        <circuit-display-container :circuit-element-str="circuitElementStr" :init-render-options="initRenderOptions">
+        </circuit-display-container>
       </div>
       <div id="circuitJSON">${JSON.stringify(CircuitStories[args.circuitPreset].args.circuit)}</div>
     </div>`
