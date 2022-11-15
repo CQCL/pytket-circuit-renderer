42862520f8bb2dd8c1b2485bf3539635660f6d0a
tiffany.duneau <tiffany@incubiq.com>
2022-11-15 16:49:52 +0000

    dark theme - update measure gate

--- src/components/circuitDisplay/gateMeasure.vue
+++ src/components/circuitDisplay/gateMeasure.vue
@@ -5,14 +5,14 @@ export default {
 </script>
 
 <template>
-  <div class="gate_measure">
+  <div class="gate_measure z">
     <svg
       xmlns='http://www.w3.org/2000/svg'
       viewBox='0 0 64 64'
       width='64' height='64'
-      fill='#fff'
-      stroke="#bbb"
-      stroke-width="4px"
+      fill='var(--box-col)'
+      stroke="var(--wire-col)"
+      stroke-width="calc(var(--spider-border) * 2)"
     >
       <path d='M63,63 C63,32 48,16 32,16 C16,16 1,32 1,63 L63,63' />
       <path d="M32,63 L56,16" />
