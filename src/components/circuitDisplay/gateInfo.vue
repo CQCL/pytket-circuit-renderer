64cfa11a82ac1a11ac8279af03b1d78b8ea48481
tiffany.duneau <tiffany@incubiq.com>
2023-10-17 17:24:18 +0100

    gateInfo support for Pauli Pair/CommutingSet boxes.

--- src/components/circuitDisplay/gateInfo.vue
+++ src/components/circuitDisplay/gateInfo.vue
@@ -37,6 +37,7 @@ export default {
       contentOps: [
         'Unitary1qBox', 'Unitary2qBox', 'Unitary3qBox',
         'ExpBox', 'PauliExpBox', 'ClassicalExpBox',
+        'PauliExpPairBox', 'PauliExpCommutingSetBox',
         'PhasePolyBox', 'CircBox', 'Conditional',
         'Custom', 'CustomGate', 'Composite', 'CompositeGate',
         'ProjectorAssertionBox', 'StabiliserAssertionBox',
@@ -161,11 +162,47 @@ export default {
             </chart-def>
 
             <div v-if="displayOp.type === 'PauliExpBox'">
-              <chart-def title="Phase" hover>
+              <chart-def title="Paulis">
+                Phase
+              </chart-def>
+              <chart-def hover>
+                <template #title>
+                  <chart-list :chart="displayOp.box.paulis"></chart-list>
+                </template>
                 <mathjax-content :formula="'\`' + displayOp.box.phase + '\`'"></mathjax-content>
               </chart-def>
-              <chart-def title="Paulis" hover>
-                <chart-list :chart="displayOp.box.paulis"></chart-list>
+              <chart-def v-if="displayOp.box.cx_config" title="Config" hover>
+                [[# displayOp.box.cx_config #]]
+              </chart-def>
+            </div>
+
+            <div v-if="displayOp.type === 'PauliExpPairBox'">
+              <chart-def title="Paulis">
+                Phase
+              </chart-def>
+              <chart-def v-for="(paulis, i) in displayOp.box.paulis_pair" :key="i" hover>
+                <template #title>
+                  <chart-list :chart="paulis"></chart-list>
+                </template>
+                <mathjax-content :formula="'\`' + displayOp.box.phase_pair[i] + '\`'"></mathjax-content>
+              </chart-def>
+              <chart-def v-if="displayOp.box.cx_config" title="Config" hover>
+                [[# displayOp.box.cx_config #]]
+              </chart-def>
+            </div>
+
+            <div v-if="displayOp.type === 'PauliExpCommutingSetBox'">
+              <chart-def title="Pauli Gadgets">
+                Phase
+              </chart-def>
+              <chart-def v-for="(gadget, i) in displayOp.box.pauli_gadgets" :key="i" hover>
+                <template #title>
+                  <chart-list :chart="gadget[0]"></chart-list>
+                </template>
+                <mathjax-content :formula="'\`' + gadget[1] + '\`'"></mathjax-content>
+              </chart-def>
+              <chart-def v-if="displayOp.box.cx_config" title="Config" hover>
+                [[# displayOp.box.cx_config #]]
               </chart-def>
             </div>
 
