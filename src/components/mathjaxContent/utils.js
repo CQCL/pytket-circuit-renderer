aa3e5fe119d68d7f316bc274ed098f537a81a512
tiffany.duneau <tiffany@incubiq.com>
2024-07-16 11:35:10 +0200

    linting

--- src/components/mathjaxContent/utils.js
+++ src/components/mathjaxContent/utils.js
@@ -7,14 +7,9 @@ import sympyParser from '@/sympyParser/sympy'
 export const coerceSympyTex = function (formula, options) {
   try {
     const parsed = sympyParser.parse(formula, options)
-    console.log("PARSING SUCCESS", formula, "->", parsed)
     return parsed
   } catch (e) {
-    if (typeof e.format === "function") {
-      console.warn("PARSING ERROR", formula, e.format([]))
-    } else console.warn("PARSING ERROR", formula, e)
-
     // Escape the original formula as plaintext.
-    return `\\text{${options.crop ? formula.slice(0,4) + '...' : formula}}`
+    return `\\text{${options.crop ? formula.slice(0, 4) + '...' : formula}}`
   }
 }
