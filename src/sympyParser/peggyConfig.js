7fb57b966666cce7882ea26566ade62a0d7979b1
tiffany.duneau <tiffany@incubiq.com>
2024-07-22 16:53:00 +0100

    refactor parser

---
+++ src/sympyParser/peggyConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  format: "commonjs",
+  input: [
+    "src/sympyParser/sympy.pegjs",
+    "src/sympyParser/sympyMath.pegjs",
+    "src/sympyParser/sympyLogic.pegjs",
+    "src/sympyParser/sympyTerminals.pegjs",
+    "src/sympyParser/sympySets.pegjs"
+  ]
+}
