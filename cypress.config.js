7a8af1c275d764c991c2278b98293d18427ebeb6
tiffany.duneau <tiffany@incubiq.com>
2024-11-19 18:54:19 +0000

    more lenient image diffs

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,26 @@
 const { defineConfig } = require('cypress')
+const webpack = require('webpack')
 
 module.exports = defineConfig({
   component: {
     devServer: {
       framework: 'vue-cli',
-      bundler: 'webpack'
+      bundler: 'webpack',
+      webpackConfig: {
+        plugins: [
+          new webpack.ProvidePlugin({
+            Buffer: ['buffer', 'Buffer']
+          })
+        ],
+        resolve: {
+          fallback: {
+            stream: require.resolve('stream-browserify'),
+            zlib: require.resolve('browserify-zlib'),
+            path: require.resolve('path-browserify'),
+            buffer: require.resolve('buffer/')
+          }
+        }
+      }
     }
   }
 })
